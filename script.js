a677beee00b4570a302c4b9caa0e27bd0feaefb9
stuti_43 <stuti.mohgaonkar@gmail.com>
2021-02-16 08:14:59 -0500

    assign tasks

---
+++ script.js
@@ -0,0 +1,37 @@
+var db = firebase.firestore();
+import tasks from './tasksmaster.js'
+let today = new Date('2017-01-01');
+let users = ['Stuti','Rajshree'];
+let removedTasks = []
+let finalTasks = []
+db.collection("chores").get().then((querySnapshot) => {
+    tasks.forEach((task) => {
+        querySnapshot.forEach((doc,index) => {
+            console.log('HAHAHAH',doc.data().Task,task.id)
+            if(task.id == doc.data().Task) {
+                console.log('HMMM')
+                removedTasks.push(task.id);
+            }
+        });
+    })
+    tasks.forEach((tasks,index) => {
+        if(removedTasks.indexOf(tasks.id) < 0) {
+            finalTasks.push(tasks)
+        }
+    })
+    let pickedIndex = Math.floor(Math.random()*(finalTasks.length));
+    let pickedUser = Math.floor(Math.random()*(users.length));
+    let textcontainer = document.getElementById('task')
+    textcontainer.innerHTML =  users[pickedUser] + ' ' + finalTasks[pickedIndex].taskText;
+    db.collection("chores").add({
+        AssignedOn: new Date(),
+        AssignedTo: users[pickedUser],
+        Task: tasks[pickedIndex].id
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+});
---
+++ tasksmaster.js
@@ -0,0 +1,25 @@
+export default [
+    {
+        "id":1,
+        "isPrequisite":true, 
+        "taskText":"Pick what needs to be cooked"
+    }, 
+    {
+        "id":2,
+        "isPrequisite":true, 
+        "taskText":"is cooking today!"
+    }, 
+    {
+        "id":3,
+        "isPrequisite":true, 
+        "taskText":"will only sleep today!"
+    }, {
+        "id":4,
+        "isPrequisite":true, 
+        "taskText":"is cleaning next!"
+    },{
+        "id":5,
+        "isPrequisite":true, 
+        "taskText":"gets groceries next"
+    }
+]
